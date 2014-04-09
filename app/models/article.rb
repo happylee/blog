393db614b012d74951b998e803fabb0dfadc74aa
Li Sui <leesui0207@gmail.com>
2014-04-09 18:54:22 +1200

    implemented search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
   	validates :title, presence: true,
                       length: { minimum: 5 }
+    def self.search( search )
+	    where("text like ?", "%#{search}%")
+	end
 end
