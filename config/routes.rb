562c8182d82cc43c1be0ebdaca413de6646b93a5
Li Sui <leesui0207@gmail.com>
2014-04-11 11:47:20 +1200

    root to index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Blog::Application.routes.draw do
   resources :articles do
      resources :comments
    end
+   root "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
